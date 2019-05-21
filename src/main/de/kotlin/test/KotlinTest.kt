00e87b59c638380de7ed2133cc8acc3659168d2b
Karl-Erik Kley <karl.erik.kley@gmail.com>
2019-05-21 12:36:22 +0200

    Add percentage overloaded function

--- src/main/de/kotlin/test/KotlinTest.kt
+++ src/main/de/kotlin/test/KotlinTest.kt
@@ -1,15 +1,31 @@
 package main.de.kotlin.test
 
-data class Money(val amount: Int, val currency: String)
+import java.lang.IllegalArgumentException
+import java.math.BigDecimal
+
+data class Money(val amount: BigDecimal, val currency: String)
+
+fun sum(x: Int, y: Int) = x + y
+
+fun convertToDollars(money: Money): Money {
+    return when (money.currency){
+        "$" ->  money
+        "Euro" -> Money(money.amount * BigDecimal(1.10),"$2")
+        else -> throw IllegalArgumentException("Not the right currency")
+    }
+}
+fun BigDecimal.percent(percentage: Int) = this.multiply(BigDecimal(percentage)).divide(BigDecimal(100))
 
 fun main() {
-    val tickets = Money(100,"$")
-    val popcorn = tickets.copy(500, "Euro ")
+    val tickets = Money(BigDecimal(100),"$")
+    val popcorn = tickets.copy(BigDecimal(500), "Euro ")
 
     if (tickets != popcorn) {
         println("different ")
     }
 
-}
+    val bd1 = BigDecimal(100)
 
+    println(bd1.percent(7))
+}
 
